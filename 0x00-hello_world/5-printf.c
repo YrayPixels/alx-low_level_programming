a4ca82abe9cf2b71c26c9cacc8834ca2f73efe7b
YrayPixels <moseserhinyodavwe2@gmail.com>
2022-06-30 19:53:03 -0700

    num5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - to print a string
+ * Description: we are using printf to print
+ * Return: 0
+ */
+int main(void)
+{
+printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
