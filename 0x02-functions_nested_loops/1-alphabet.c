9296447e1138f5531f37efef2001a354c80e7bb3
YrayPixels <moseserhinyodavwe2@gmail.com>
2022-07-06 15:50:33 -0700

    tried num 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* print_alphabet - this file prints all the alphabet in lowercase
+*
+*Return 0
+*/
+void print_alphabet(void)
+{
+   char c;
+for (c ='a'; c<= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
